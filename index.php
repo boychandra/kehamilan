2fa57a67fe10b31dad10c21d774055e9efc3bac8
boychandra <boy.chandra30@gmail.com>
2019-04-24 06:36:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,41 @@
-<?php
+<?php 
 
-header("Content-Type: application/json");
-ob_start();
-$requestBody = file_get_contents('php://input'); 
-$json = json_decode($requestBody, true);
+$method = $_SERVER['REQUEST_METHOD'];
 
-$text = $json['result']['resolvedQuery'];
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
 
-if (strpos(strtolower($text), "ram") !== false) {
-	$displayText = "Hello Ram, this is from php.";
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
-else {
-	$displayText = "Hello Ram, this is else part.";
+else
+{
+	echo "Method not allowed";
 }
-
-$response = json_encode(array(
-           		"source" => "webhook",
-           		"speech" => $displayText,
-           		"displayText" => $displayText,
-           		"contextOut" => array()
-       		));
-
-ob_end_clean();
-echo $response;
 
 ?>
-
-
