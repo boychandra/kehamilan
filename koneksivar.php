54e3a00f99905c41399fb165301ffcde4851709d
boychandra <boy.chandra30@gmail.com>
2019-04-24 06:13:17 +0700

    Add files via upload

---
+++ koneksivar.php
@@ -0,0 +1,18 @@
+<?php
+
+$DBServer = 'localhost';
+$DBUser   = 'root';
+$DBPass   = '';
+$DBName   = 'db_chatbot';
+$gambar0="avatar.jpg";
+$css="greenblack.css";//greenblack,gradient,flickr,amazon
+$PATH="ypathcss";
+$YPATH="ypathfile";
+
+$tittle="CLIENT SERVER";
+$header="Client Server";
+$footer="Http://www.client-server.org";
+
+$tbadmin="tb_admin";
+$tbuser="tb_user";
+?>
